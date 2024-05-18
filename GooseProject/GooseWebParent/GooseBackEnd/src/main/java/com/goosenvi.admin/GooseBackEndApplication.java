5eb083910df195d9621a39e54998fbbaab57211d
Thapelow <thapelowmakhanya@gmail.com>
2024-05-18 17:10:05 +0200

    Added Role Entity and Repository

--- GooseProject/GooseWebParent/GooseBackEnd/src/main/java/com/goosenvi.admin/GooseBackEndApplication.java
+++ GooseProject/GooseWebParent/GooseBackEnd/src/main/java/com/goosenvi.admin/GooseBackEndApplication.java
@@ -1,9 +1,12 @@
 package com.goosenvi.admin;
 
+import jakarta.persistence.Entity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({"com.goosenvi.common.entity", "com.goosenvi.admin.user"})
 public class GooseBackEndApplication {
 
 	public static void main(String[] args) {
